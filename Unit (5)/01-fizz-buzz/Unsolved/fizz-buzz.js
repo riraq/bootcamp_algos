b95d9224a12082499d7d0cc4769568cafe88112b
riraq <riraq@mail.usf.edu>
2021-06-25 17:09:12 -0500

    completed 5-01

--- Unit (5)/01-fizz-buzz/Unsolved/fizz-buzz.js
+++ Unit (5)/01-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,19 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function (arr) {
+
+  for (let i = 0; i < arr.length; i++) {
+    let arrValue = arr[i];
+
+    if (arrValue % 3 === 0 && arrValue % 5 !== 0) {
+      console.log("Fizz")
+    } else if (arrValue % 5 === 0 && arrValue % 3 !== 0) {
+      console.log("Buzz")
+    } else if (arrValue % 5 === 0 && arrValue % 3 === 0) {
+      console.log("Fizz Buzz")
+    } else {
+      console.log(arrValue)
+    }
+  }
+};
