7f1dff149b631f1b16bc32c0bae7dbef1648cc3d
riraq <riraq@mail.usf.edu>
2021-06-25 15:37:25 -0500

    completed Unit 3-03

--- Unit (3)/03-log-nums/Unsolved/log-nums.js
+++ Unit (3)/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,9 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
+var logNums = function(num) {
+  
+  for (let i=1; i<num+1; i++){
+    console.log(i)
+  }
 
-var logNums = function(num) {};
+};
