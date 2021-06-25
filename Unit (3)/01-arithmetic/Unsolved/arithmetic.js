245ada221188880013e5695cce779ffb07f3f091
riraq <riraq@mail.usf.edu>
2021-06-25 14:08:25 -0500

    completed Unit 3-01

--- Unit (3)/01-arithmetic/Unsolved/arithmetic.js
+++ Unit (3)/01-arithmetic/Unsolved/arithmetic.js
@@ -1,9 +1,21 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
+// returns the result of num1 plus num2.
+var add = function(num1, num2) {
+  return num1 + num2
+};
 
-var subtract = function(num1, num2) {};
+// subtract: returns the result of num1 minus num2.
+var subtract = function(num1, num2) {
+  return num1 - num2
+};
 
-var multiply = function(num1, num2) {};
+// multiply: returns the result of num1 times num2.
+var multiply = function(num1, num2) {
+  return num1 * num2
+};
 
-var divide = function(num1, num2) {};
+// divide: returns the result of num1 divided by num2.
+var divide = function(num1, num2) {
+  return num1 / num2
+};
