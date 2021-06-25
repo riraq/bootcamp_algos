535b0232e2e84d3f589e4fd3e8e8a5fdd0a6c2c6
riraq <riraq@mail.usf.edu>
2021-06-25 17:20:20 -0500

    completed 5-02

--- Unit (5)/02-max-num/Unsolved/max-num.js
+++ Unit (5)/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,16 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function (arr) {
+
+  let maxNumber;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      maxNumber = arr[i]
+    } else if (arr[i] > maxNumber) {
+      maxNumber = arr[i]
+    };
+  };
+  
+  return maxNumber;
+};
