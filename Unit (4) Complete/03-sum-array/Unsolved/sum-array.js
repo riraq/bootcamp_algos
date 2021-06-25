85170076b5c66f1b3996f98910356425061557c4
riraq <riraq@mail.usf.edu>
2021-06-25 16:15:48 -0500

    completed 4-03

---
+++ Unit (4) Complete/03-sum-array/Unsolved/sum-array.js
@@ -0,0 +1,15 @@
+// Write code to add all the numbers in `arr` and return the total
+
+var sumArray = function(arr) {
+
+  let total = 0;
+
+  for(let i = 0; i<arr.length; i++){
+
+    total += arr[i]
+
+  };
+
+  return total
+
+};
