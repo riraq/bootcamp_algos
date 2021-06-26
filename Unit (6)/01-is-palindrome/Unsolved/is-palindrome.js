aebaee0f1ece56af5b3dd50813d60324e15862b0
riraq <riraq@mail.usf.edu>
2021-06-25 19:50:20 -0500

    completed 6-01

--- Unit (6)/01-is-palindrome/Unsolved/is-palindrome.js
+++ Unit (6)/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,19 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function (str) {
+  // turns the string into an array
+  let strArr = str.split('')
+  // turns the string into an array, then reverses it
+  let reversed = str.split('').reverse();
+
+  // compares each index of both arrays 
+  // returns false at the first instance of a non-matching character 
+  for (let i = 0; i < reversed.length; i++) {
+    if (reversed[i] !== strArr[i]) {
+      return false
+    }
+  }
+
+  // returns true if for loop does not return false
+  return true
+};
