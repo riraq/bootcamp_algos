c30226b4174ee88ba9fdc628b0347e270e543f08
riraq <riraq@mail.usf.edu>
2021-07-06 17:38:40 -0500

    completed 9-02

--- Unit (9)/01-character-count/Unsolved/character-count.js
+++ Unit (9)/01-character-count/Unsolved/character-count.js
@@ -1,3 +1,8 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
+// Create a new object.
+// Loop through each character in the given string and create a new key for each character in the string.
+// The value of the character's key should be the number of times it appears in the string.
 
-var characterCount = function(str) {};
+var characterCount = function (str) {
+  
+};
--- Unit (9)/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ Unit (9)/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,17 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function (arr) {
+  let first = arr[0];
+  let second = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > first) {
+      second = first
+      first = arr[i]
+    } else if (arr[i] > second) {
+      second = arr[i]
+    };
+  }
+
+  return first * second
+};
