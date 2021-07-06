735906e95db83b0778d355c1005c23462a2e0857
riraq <riraq@mail.usf.edu>
2021-07-06 18:14:53 -0500

    complete 9-01; added comments to 9-02

--- Unit (9)/01-character-count/Unsolved/character-count.js
+++ Unit (9)/01-character-count/Unsolved/character-count.js
@@ -1,8 +1,22 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
-// Create a new object.
-// Loop through each character in the given string and create a new key for each character in the string.
-// The value of the character's key should be the number of times it appears in the string.
 
 var characterCount = function (str) {
-  
+  // creating object that will hold counts of each letter
+  let counts = {};
+
+  // loop to go through each letter in the string
+  for (let i = 0; i < str.length; i++) {
+
+    // checking if the letter is already in the object as a key
+    // if -> increment the value by 1
+    // else -> add the letter as a key with a value of 1
+    if (str[i] in counts) {
+      counts[str[i]]++
+    } else {
+      counts[str[i]] = 1
+    }
+  }
+
+  // return object with key value pairs of each letter from string
+  return counts;
 };
--- Unit (9)/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ Unit (9)/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,10 +1,18 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
 var productOfLargestTwo = function (arr) {
+  // setting variables to hold largest and second largest values
+  // immediately adding first value to largest variable to skip the loop check
   let first = arr[0];
   let second = 0;
 
+  // loop to check whether the number is larger than what is currently in the variables
   for (let i = 1; i < arr.length; i++) {
+    // if -> number is larger than the current largest number,
+    // move the current largest value to second largest value
+    // put new largest value in first variable
+    // else if -> number is larger than second variable,
+    // replace variable. Otherwise, do nothing.
     if (arr[i] > first) {
       second = first
       first = arr[i]
@@ -13,5 +21,6 @@ var productOfLargestTwo = function (arr) {
     };
   }
 
+  // return product of two largest numbers
   return first * second
 };
